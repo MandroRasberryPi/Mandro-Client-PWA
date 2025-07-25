ea3da639e94c6be2bc833496f8084d38354edd1a
psm1st <psm1st@sookmyung.ac.kr>
2025-07-25 11:15:46 +0900

    chore : tailwind css 세팅 완료

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-yellow-200">
+      <h1 className="text-4xl font-bold text-red-500">
+        Tailwind CSS 간단 테스트!
+      </h1>
+    </main>
+  );
+}
