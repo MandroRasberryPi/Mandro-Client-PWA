842bbc821f61fe6655bc2d91dd81224b3e25c75b
psm1st <psm1st@sookmyung.ac.kr>
2025-07-28 17:32:10 +0900

    chore : description 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Mandro Front",
-  description: "Tailwind with Next.js app router",
+  title: "Mandro",
+  description: "Mandro raspberrypi support Application",
 };
 
 export default function RootLayout({
@@ -12,6 +12,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </head>
       <body className="min-h-screen bg-gray-50 text-gray-900 antialiased">
         {children}
       </body>
