df016d4dc5932a9dfcca5d38a8d07bdbe8beb4e0
psm1st <psm1st@sookmyung.ac.kr>
2025-07-29 09:19:50 +0900

    fix : service worker 부분 로직 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import "./globals.css";
+import { useEffect } from "react";
 
 export const metadata = {
   title: "Mandro",
@@ -10,6 +13,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((registration) => {
+          console.log("Service Worker 등록 완료:", registration);
+        })
+        .catch((error) => {
+          console.error("Service Worker 등록 실패:", error);
+        });
+    }
+  }, []);
+
   return (
     <html lang="en">
       <head>
